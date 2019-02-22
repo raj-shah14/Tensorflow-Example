695ffa400cbfa3b795cc925b1dac291dad45d559
Raj Shah <rajpareshshah14@gmail.com>
2019-02-22 17:27:22 -0600

    Parallel multi GPU training. (Distributed)

---
+++ train_tfrecord_gpu.py
@@ -0,0 +1,135 @@
+import tensorflow as tf
+import os
+import cv2
+import sys
+import numpy as np
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+#sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def parser(record):
+    keys_to_features = {
+        "image_raw": tf.FixedLenFeature([], tf.string),
+        "label": tf.FixedLenFeature([], tf.int64)
+    }
+    parsed = tf.parse_single_example(record, keys_to_features)
+    image = tf.decode_raw(parsed["image_raw"], tf.uint8)
+    image = tf.cast(image, tf.float32)
+    image = tf.reshape(image, shape=[224, 224, 3])
+    label = tf.cast(parsed["label"], tf.int32)
+
+    return {'image': image}, label
+
+
+def input_fn(filenames):
+    dataset = tf.data.TFRecordDataset(filenames=filenames, num_parallel_reads=40)
+    dataset = dataset.apply(
+      tf.contrib.data.shuffle_and_repeat(1024, 1)
+    )
+    dataset = dataset.apply(
+      tf.contrib.data.map_and_batch(parser, 32)
+    )
+    #dataset = dataset.map(parser, num_parallel_calls=12)
+    #dataset = dataset.batch(batch_size=1000)
+    dataset = dataset.prefetch(buffer_size=2)
+    return dataset
+
+
+def train_input_fn():
+    return input_fn(filenames=["train.record","test.record"])
+
+
+def val_input_fn():
+    return input_fn(filenames=["val.record"])
+
+def model_fn(features, labels, mode, params):
+    num_classes = 2
+    net = features["image"]
+    net = tf.identity(net, name="input_tensor")
+    net = tf.reshape(net, [-1, 224, 224, 3])    
+    net = tf.identity(net, name="input_tensor_after")
+
+    net = tf.layers.conv2d(inputs=net, name='layer_conv1',
+                           filters=32, kernel_size=3,
+                           padding='same', activation=tf.nn.relu)
+    net = tf.layers.max_pooling2d(inputs=net, pool_size=2, strides=2)
+    net = tf.layers.conv2d(inputs=net, name='layer_conv2',
+                           filters=64, kernel_size=3,
+                           padding='same', activation=tf.nn.relu)
+    net = tf.layers.max_pooling2d(inputs=net, pool_size=2, strides=2)  
+    net = tf.layers.conv2d(inputs=net, name='layer_conv3',
+                           filters=64, kernel_size=3,
+                           padding='same', activation=tf.nn.relu)
+    net = tf.layers.max_pooling2d(inputs=net, pool_size=2, strides=2)    
+    net = tf.contrib.layers.flatten(net)
+    net = tf.layers.dense(inputs=net, name='layer_fc1',
+                        units=128, activation=tf.nn.relu)  
+    net = tf.layers.dropout(net, rate=0.5, noise_shape=None, 
+                        seed=None, training=(mode == tf.estimator.ModeKeys.TRAIN))
+    net = tf.layers.dense(inputs=net, name='layer_fc_2',
+                        units=num_classes)
+
+    logits = net
+    y_pred = tf.nn.softmax(logits=logits)
+
+    y_pred = tf.identity(y_pred, name="output_pred")
+    y_pred_cls = tf.argmax(y_pred, axis=1)
+    y_pred_cls = tf.identity(y_pred_cls, name="output_cls")
+
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        spec = tf.estimator.EstimatorSpec(mode=mode,
+                                          predictions=y_pred_cls)
+    else:
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels,
+                                                                       logits=logits)
+        loss = tf.reduce_mean(cross_entropy)
+
+        optimizer = tf.train.AdamOptimizer(learning_rate=params["learning_rate"])
+        train_op = optimizer.minimize(
+            loss=loss, global_step=tf.train.get_global_step())
+        metrics = {
+            "accuracy": tf.metrics.accuracy(labels, y_pred_cls)
+        }
+
+        spec = tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            train_op=train_op,
+            eval_metric_ops=metrics)
+    return spec
+
+# Download NCCL library: https://developer.nvidia.com/nccl/nccl-download
+# sudo apt-get update
+# Check Cuda version using nvcc --version
+# sudo apt install libnccl2=2.4.2-1+cuda9.0 libnccl-dev=2.4.2-1+cuda9.0
+
+strategy = tf.contrib.distribute.MirroredStrategy(num_gpus=7)
+config = tf.estimator.RunConfig(train_distribute=strategy, eval_distribute=strategy)
+
+model=tf.estimator.Estimator(model_fn=model_fn,
+                               params={"learning_rate": 1e-4},
+                               model_dir="./output_gpu/",
+                               config=config)
+
+count=1
+while (count < 101):
+    model.train(input_fn=train_input_fn, steps=1000)
+    result = model.evaluate(input_fn=val_input_fn)
+    print(result,count)
+    print("Classification accuracy: {0:.2%}".format(result["accuracy"]))
+    sys.stdout.flush()
+    count = count + 1
+
+
+
